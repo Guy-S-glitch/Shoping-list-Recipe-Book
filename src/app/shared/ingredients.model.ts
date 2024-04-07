262b1223742ab70f83937013aed68f9366dd7c9f
Guy-S-glitch <guysolomon365@gmail.com>
2024-04-07 23:03:39 +0300

    added ingredients shared class

---
+++ src/app/shared/ingredients.model.ts
@@ -0,0 +1,5 @@
+export class ingredients {
+    constructor(private Name: string, private Amount: number) { }
+    public GetName = () => { return this.Name; }
+    public GetAmount = () => { return this.Amount; }
+}
