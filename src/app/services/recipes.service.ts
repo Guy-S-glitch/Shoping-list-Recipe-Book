0e24eded7ab206fca153532c661523747f784a6c
Guy-S-glitch <guysolomon365@gmail.com>
2024-04-13 15:16:30 +0300

    styling active recipe item

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -14,7 +14,7 @@ export class RecipesService {
       [new ingredients('car', 1), new ingredients('banana', 6)]
     ),
     new recipeModel(
-      'Snitzelim',
+      'OTher shi',
       'goood',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyKIfkDVw9KqcWp0DPYCDhl4GA98hRqfDf1WQenMbvif6OUZtHVdxwM-O1aFcZowmbdCg&usqp=CAU',
       [new ingredients('car', 1), new ingredients('banana', 6)]
