0c2381c78c303390d75a6ce811ceb404861f3851
Guy-S-glitch <guysolomon365@gmail.com>
2024-04-13 15:05:26 +0300

    able to navigate to recipe detail through routing

--- src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,20 +1,29 @@
 import { ShoppingService } from './../../../services/shopping.service';
 import { RecipesService } from '../../../services/recipes.service';
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { recipeModel } from '../../../Models/recipe.model';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css',
 })
-export class RecipeDetailComponent {
-  @Input() GetSelectedItem: recipeModel;
+export class RecipeDetailComponent implements OnInit {
+  GetSelectedItem: recipeModel;
   private recipesService: RecipesService;
   private shoppingService: ShoppingService;
-  constructor() {
+  id: number;
+  constructor(private router: Router, private route: ActivatedRoute) {
     this.recipesService = inject(RecipesService);
     this.shoppingService = inject(ShoppingService);
+  }
+  ngOnInit(): void {
+    this.id = +this.route.snapshot.params['id'];
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.GetSelectedItem = this.recipesService.GetRecipes()[this.id];
+    });
   }
   SendToIngredients() {
     this.shoppingService.AddIngredientsFromShoppingList(
