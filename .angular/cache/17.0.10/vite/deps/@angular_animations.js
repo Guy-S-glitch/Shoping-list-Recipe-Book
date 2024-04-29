6995ca542ace8f5df85ff924a7e766629a9cafec
Guy-S-glitch <guysolomon365@gmail.com>
2024-04-29 16:33:45 +0300

    drop files from gitignore

---
+++ .angular/cache/17.0.10/vite/deps/@angular_animations.js
@@ -0,0 +1,52 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-7UEXXVDU.js";
+import "./chunk-LWT6Q6KX.js";
+import "./chunk-O7G35XVC.js";
+import "./chunk-BGHTEUUK.js";
+import "./chunk-EEPPBFIQ.js";
+import "./chunk-DD56SOZX.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
